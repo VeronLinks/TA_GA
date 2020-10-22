645dd514ea3e8acbd5dcb25924e7761d1b68582b
Eduardo <edusabix@gmail.com>
2020-10-22 12:59:13 +0200

    Client Actions Managing

--- HypermediaExercise_Client/src/index.ts
+++ HypermediaExercise_Client/src/index.ts
@@ -1,2 +1,42 @@
+import { Console } from "console";
+
 const req = require("request");
 const read = require("readline");
+
+var actions = [
+    "Bebi mucho loco no me acuerdo", "Me folle a un travesti" ,"Me pote en la mascarilla"
+    //Aqui se supone que hacemos una request de las opciones almacenadas en la dataBase, 
+    //cada turno hace un print de 3 acciones (string) y van cambiando segun el stage (int)
+];
+
+const readLines = read.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  console.log("-Oh its seems like you were wasted last night, let's try to remeber where did you lose your keys.");
+  console.log("-What is the last thing you remember?");
+  printActions();
+
+  function selectAction(action)
+  {
+
+  }
+
+  function readAnswer(answer)
+  {
+    var action = parseInt(answer);
+    if(action < actions.length && action >= 0)
+    {
+      selectAction(action);
+    }
+  }
+
+  function printActions(){
+    console.log("Select one action: \n");
+
+    for (let i = 0; i < actions.length; i++) {
+      const actionText = actions[i];
+      console.log(i + " : " + actionText);
+    }
+  }
